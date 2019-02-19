21876aea22374a26ed70fddf80add1318ea03efa
wuyxiquanquan <wyxiang@stu.hunau.edu.cn>
2019-02-19 09:35:25 +0800

    瞎玩
    Intent跳转

--- app/src/main/java/cn/edu/hunau/wyxiang/quanquan/MainActivity.java
+++ app/src/main/java/cn/edu/hunau/wyxiang/quanquan/MainActivity.java
@@ -1,9 +1,11 @@
 package cn.edu.hunau.wyxiang.quanquan;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TabHost;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +19,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "wtf", Toast.LENGTH_SHORT).show();
+            }
+        });
+        Button button2 = findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Button2.class);
+                startActivity(intent);
+//                Toast.makeText(MainActivity.this, Button2.class.toString(), Toast.LENGTH_SHORT).show();
+
             }
         });
 
