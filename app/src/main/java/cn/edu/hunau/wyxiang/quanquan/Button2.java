ac3f125699d8d7f27d78f02cd8c0ad62f6d51710
wuyxiquanquan <wyxiang@stu.hunau.edu.cn>
2019-02-19 21:19:51 +0800

    2.18

--- app/src/main/java/cn/edu/hunau/wyxiang/quanquan/Button2.java
+++ app/src/main/java/cn/edu/hunau/wyxiang/quanquan/Button2.java
@@ -3,11 +3,49 @@ package cn.edu.hunau.wyxiang.quanquan;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import io.fotoapparat.Fotoapparat;
+import io.fotoapparat.log.LoggersKt;
+import io.fotoapparat.parameter.ScaleType;
+import io.fotoapparat.selector.FlashSelectorsKt;
+import io.fotoapparat.selector.FocusModeSelectorsKt;
+import io.fotoapparat.selector.LensPositionSelectorsKt;
+import io.fotoapparat.selector.ResolutionSelectorsKt;
+import io.fotoapparat.selector.SelectorsKt;
+import io.fotoapparat.view.CameraView;
+
 public class Button2 extends AppCompatActivity {
+
+    private CameraView cameraView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button2);
+
+        cameraView = findViewById(R.id.camera_view);
+
+
+        Fotoapparat
+                .with(this)
+                .into(cameraView)           // view which will draw the camera preview
+                .previewScaleType(ScaleType.CenterCrop)  // we want the preview to fill the view
+                .photoResolution(ResolutionSelectorsKt.highestResolution())   // we want to have the biggest photo possible
+                .lensPosition(LensPositionSelectorsKt.back())       // we want back camera
+                .focusMode(SelectorsKt.firstAvailable(  // (optional) use the first focus mode which is supported by device
+                        FocusModeSelectorsKt. continuousFocusPicture(),
+                        FocusModeSelectorsKt.autoFocus(),        // in case if continuous focus is not available on device, auto focus will be used
+                        FocusModeSelectorsKt.fixed()             // if even auto focus is not available - fixed focus mode will be used
+                ))
+                .flash(SelectorsKt.firstAvailable(      // (optional) similar to how it is done for focus mode, this time for flash
+                        FlashSelectorsKt.autoRedEye(),
+                        FlashSelectorsKt.autoFlash(),
+                        FlashSelectorsKt.torch()
+                ))
+//                .frameProcessor(myFrameProcessor)   // (optional) receives each frame from preview stream
+                .logger(LoggersKt.loggers(            // (optional) we want to log camera events in 2 places at once
+                        LoggersKt.logcat(),           // ... in logcat
+                        LoggersKt.fileLogger(this)    // ... and to file
+                ))
+                .build();
     }
 }
